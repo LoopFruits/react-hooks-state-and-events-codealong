8c4acb29040b7b9667e9d5550c910efbe992266b
LoopFruits <Kensworldc@gmail.com>
2022-05-14 20:55:36 -0400

    completed assignment

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
+
+
 
 function Toggle() {
-  return <button>OFF</button>;
+
+  const [isOn,setIsOn] = useState(false);
+  // ... the rest of Toggle component
+  // Whenever you're using a React hook, it must be within a React component. We're setting the initial state here as false, because the button should be "OFF" when the component first renders.
+
+  function handleClick() {
+    setIsOn((isOn) => !isOn);
+  }
+
+  // if the button is on the color will be red, if not then the color will be white 
+  const color = isOn ? "red" : "white";
+
+  return (
+    
+    <button style={{ background: color }} onClick={handleClick}>
+      { isOn ? "ON" : "OFF" }
+    </button>
+    
+  );
 }
 
 export default Toggle;
+ 
